935b81b66582ff3976349382fe3199c3ba9a33c9
Atyo12 <vargatti121@gmail.com>
2026-04-14 20:34:04 +0200

    Add files via upload

---
+++ Attolo_V0_23_0_090426_Encoder_HMI_1/machine.h
@@ -0,0 +1,67 @@
+#pragma once
+#include "types.h"
+
+void machineSetupDefaults();
+void machineLoop();
+
+// HMI parancsok beállítása kívülről
+void machineSetHmiHandleMode(bool v);
+void machineSetHmiServiceEnable(bool v);
+void machinePulseHmiReset();
+
+// Return conveyor HMI parancsok
+void machineSetReturnForceEmpty(bool v);
+bool machineGetReturnForceEmpty();
+
+void machineSetReturnConveyorSpeedHz(float hz);
+float machineGetReturnConveyorSpeedHz();
+
+void machineSetReturnConveyorContinuous(bool v);
+bool machineGetReturnConveyorContinuous();
+
+// HMI státuszhoz szükséges publikus getterek
+OpMode machineGetOpMode();
+State machineGetState();
+Fault machineGetFault();
+
+bool machineIsFaultLatched();
+bool machineIsResetRequestActive();
+
+bool machineGetHandleMode();
+bool machineGetServiceEnable();
+
+bool machineIsPushActive();
+bool machineIsReturnConveyorRunning();
+bool machineIsWashStopRequested();
+
+float machineGetReturnActualHz();
+
+int32_t machineGetReturnLoad();
+
+// Fan1 damper státusz / HMI getterek
+float machineGetFan1DamperCmdPct();
+float machineGetFan1DamperFbPct();
+bool  machineIsFan1DamperOpenCmd();
+bool  machineIsFan1DamperOpenFb();
+bool  machineIsFan1RunPermitted();
+bool  machineIsFan1Running();
+
+//légtechnika
+bool ventilationAutoAllowed();
+
+// Paraméterek
+const MachineParams& machineGetParams();
+void machineSetParams(const MachineParams& p);
+void machineResetParamsToDefaults();
+bool machineLoadParamsFromEeprom();
+void machineSaveParamsToEeprom();
+
+void machineSetClimateTestOverride(bool en);
+bool machineGetClimateTestOverride();
+
+void machineSetClimateTestTrh1(float tempC, float rh, bool valid);
+void machineSetClimateTestTrh2(float tempC, float rh, bool valid);
+void machineSetClimateTestTrh3(float tempC, float rh, bool valid);
+
+void machineSetClimateTestTrhValid(uint8_t idx, bool valid);
+void machinePrintClimateTestState();
